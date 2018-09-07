e2dbda4168a985ec908361e42f98359fef4fa795
stoneageartisans <william.a.mann@hotmail.com>
2018-09-06 21:28:13 -0500

    Initial Unity project files.

---
+++ WMann_UATanks/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("ERROR: An instance of " + gameObject.name + " already exists.");
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
